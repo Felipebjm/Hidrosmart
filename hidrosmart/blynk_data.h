e141135da79aa93d19e1918824cf3fd9fdbce541
Felipebjm <febejaranom@gmail.com>
2025-10-16 14:03:01 -0600

    Asignacion de pines virtuales y codigo del .ino

--- hidrosmart/blynk_data.h
+++ hidrosmart/blynk_data.h
@@ -6,3 +6,27 @@
 #define BLYNK_TEMPLATE_NAME "Hidrosmart"
 #define BLYNK_AUTH_TOKEN "KObwQyFNI_MoKEiU3b_OSBnk__WOF6fe"
 #define BLYNK_PRINT Serial   // Comment this out to disable prints and save space
+
+//Asignacion de los pines virtuales
+//Downlink son los pines que reciben datos de Blynk
+//En blynk cada widget tien un pin asociado (v0,v1,etc..)
+#define blynk_hum1_actual 0
+#define blynk_hum2_actual 1
+#define blynk_hum3_actual 2
+#define blynk_hum1_min 3 //Downlink
+#define blynk_hum2_min 4 //Downlink
+#define blynk_hum3_min 5 //Downlink
+#define blynk_hum1_max 6 //Downlink
+#define blynk_hum2_max 7 //Downlink
+#define blynk_hum3_max 8 //Downlink
+#define blynk_estado1_riego 9  //Downlink
+#define blynk_estado2_riego 10  //Downlink
+#define blynk_estado3_riego 11  //Downlink
+#define blynk_horario1_riego 12  //Downlink
+#define blynk_horario2_riego 13  //Downlink
+#define blynk_horario3_riego 14  //Downlink
+#define blynk_probabilidad_lluvia 15
+#define blynk_limite_lluvia_z1 16
+#define blynk_limite_lluvia_z2 17
+#define blynk_limite_lluvia_z3 18
+#endif
--- hidrosmart/hidrosmart.ino
+++ hidrosmart/hidrosmart.ino
@@ -11,10 +11,67 @@
 
 #include <SPI.h> 
 #include <TFT_eSPI.h> //Biblioteca de la pantalla
+#include <blynk_data.h> 
+#include <BlynkSimpleEsp32.h> //Biblioteca de Blynk
+#include <HTTPClient.h> //Libreria
 #include <ArduinoJson.h> //Libreria para utilizar archivos json
+#include <WidgetRTC.h> //Lib para sincronizar la hora 
 #include <hidrosmart.h>
+
+//Configuracion de Blynk
+char auth[] = BLYNK_AUTH_TOKEN; //Token
+char ssid[] = ""; //Nombre de la red wifi
+char pass[] = ""; //Contrasena de la red
+
+const int  RENDER_DISPLAY_TIEMPO = 1000L; //periodo del timer del diplay en ms
+const int  WEATHER_API_QUERY_TIEMPO = 60000L; //periodo del timer en ms para solicitar los datos al API
+const int  ACT_ZONAS_TIEMPO = 500L; //periodo en ms para actualizar zonas de riego
+
+BlynkTimer hidrosmartTimer;  //Timer para tareas periodicas
+
+TFT_eSPI tft = TFT_eSPI(135, 240); 
+WidgetRTC rtc;
+
+int timerRenderDisplayId;
+int timerConsultarClimaId;
+int timerActualizarZonasId;
+
+float probabilidadLluvia = 0;
+bool blynkConnected = false; //flag que indica el estado de conexion con Blynk
+
+Clima climaCartago(API_KEY,CITY);
+ZonaRiego* zonas[3]; //Crea un array donde se almacenaran las 3 zonas
+
 void setup()
 {
+  Serial.begin(115200);
+  delay(4000);
+  Serial.println("< RIEGO INTELIGENTE >");
+  //Configuracion de los timers
+  hidrosmartTimer.setTimeout(3600000L, [] () {} ); // dummy/sacrificial Function
+  timerRenderDisplayId = hidrosmartTimer.setInterval(RENDER_DISPLAY_TIEMPO, renderDisplay); //Actualizacion del diplay
+  timerConsultarClimaId = hidrosmartTimer.setInterval(WEATHER_API_QUERY_TIEMPO,actualizarProbabiliadLLuvia); //Timer para consultar al API
+  hidrosmartTimer.disable(timerConsultarClimaId);
+  timerActualizarZonasId = hidrosmartTimer.setInterval(ACT_ZONAS_TIEMPO,actualizarZonas); //Actualizar las zona
+  hidrosmartTimer.disable(timerConsultarClimaId);
+
+  //Configuracion de la pantalla
+  tft.init();
+  tft.setRotation(1);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_GREEN);
+  tft.setTextDatum(MC_DATUM);
+  tft.setTextSize(2); 
+  tft.setCursor(0, 0);
+  tft.drawString("HIDROSMART", tft.width() / 2, tft.height() / 2 - 18);
+  tft.drawString("__________", tft.width() / 2, tft.height() / 2 );
+  tft.setTextColor(TFT_BLUE);
+  tft.drawString("ING. COMPUTADORES", tft.width() / 2, tft.height() / 2 + 18);
+  tft.drawString("TEC", tft.width() / 2, tft.height() / 2 + 34 );
+  tft.drawString("Para ver si corre", tft.width() / 2, tft.height() / 2 + 50); 
+
+
+
   
 }
 
