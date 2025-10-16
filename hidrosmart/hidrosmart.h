729a8a8c820bb85d60a251c1d5cb12bac6944496
Felipebjm <febejaranom@gmail.com>
2025-10-16 11:43:39 -0600

    Clase SensorHumedad

--- hidrosmart/hidrosmart.h
+++ hidrosmart/hidrosmart.h
@@ -10,3 +10,31 @@ const String CITY    = "Cartago,CR";  // Openweather location
 #define HUM1_PIN 39   //Pin sensor  capacitivo humedad z1
 #define HUM2_PIN 32   //Pin sensor  humedad z2
 #define HUM3_PIN 33   //Pin sensor  humedad z3
+
+class SensorHumedad
+{  
+  private:
+  int rawSeco = 4095;  //Valor raw (data sin procesar) del sensor cuando el suelo está seco
+  int rawSaturado = 100;  //Valor raw del sensor cuando el suelo esta saturado de agua
+  int pin; //Pin del sensor
+  int raw; //Raw son los volores crudos
+  int humedad;
+
+  public: SensorHumedad(){};  //metodo constructor
+  
+  void iniciar(int _pin) //Asigna el pin
+  { 
+    pin = _pin;
+  }
+
+  int obtenerHumedad() //Metodo que obtiene la humedad y procesa el resultado del sensor
+  {
+    raw = analogRead(pin); //valores obtenidos por el sensor (sin procesar)
+    int porcentaje = map(raw,rawSeco,rawSaturado,0,100); //ajusta el valor del sensor a la escala 0-100
+    int porcentajeOk = constrain(porcentaje,0,100); //Garantiza que porcentaje este entre 0 y 100
+    //Serial.printf("obtenerHumedad: | raw : %d | porcentaje : %d | porcentajeOk : %d\n",raw,porcentaje,porcentajeOk);
+    return porcentajeOk;
+  };
+};
+
+
