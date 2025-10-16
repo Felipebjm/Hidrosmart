74805954aed340a8f9d28d3874be416b55253e8f
Felipebjm <febejaranom@gmail.com>
2025-10-16 11:27:36 -0600

    Primer commit

---
+++ hidrosmart/hidrosmart.ino
@@ -0,0 +1,25 @@
+/* DESCRIPCION:
+  Proyecto: Hidrosmart
+  Hardware:
+    ESP32, marca Lyligo, modelo T-Display con pantalla TFT color, 135x240 
+   uso de Blynk para
+        - Mostrar datos de sensores de humedad
+        - Configurar horario de riego
+        - Configurar threshold de % humedad
+        - Mostrar probabilidad de lluvia desde Weather API
+ */
+
+
+void setup()
+{
+  
+}
+
+
+void loop()
+{
+  
+}
+
+
+
