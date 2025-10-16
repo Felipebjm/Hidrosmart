02a57f0f39b28b60366b3bd0a51f3c952cadec08
Felipebjm <febejaranom@gmail.com>
2025-10-16 13:31:16 -0600

    Configuracion inicial de blynk

---
+++ hidrosmart/blynk_data.h
@@ -0,0 +1,8 @@
+#ifndef blynk_data_h 
+#define blynk_data_h
+//Referencia:https://examples.blynk.cc/?board=ESP32&shield=ESP32%20WiFi&example=GettingStarted%2FPushData
+/* BLYNK INFORMACION TEMPLATE */
+#define BLYNK_TEMPLATE_ID "TMPL2dWfY6YWP"
+#define BLYNK_TEMPLATE_NAME "Hidrosmart"
+#define BLYNK_AUTH_TOKEN "KObwQyFNI_MoKEiU3b_OSBnk__WOF6fe"
+#define BLYNK_PRINT Serial   // Comment this out to disable prints and save space
