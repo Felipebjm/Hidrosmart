21c315927eb9e05020a6c89242f3ff12f1024663
Felipebjm <febejaranom@gmail.com>
2025-10-16 11:36:54 -0600

    Config de pines y API key

---
+++ hidrosmart/hidrosmart.h
@@ -0,0 +1,12 @@
+#ifndef hidrosmart_h 
+#define hidrosmart_h
+
+// Hidrosmart configuracion
+const String API_KEY = "1d8f3dd4721fe20a2224b286112fe0ec"; // OpenWeatherMap apikey
+const String CITY    = "Cartago,CR";  // Openweather location
+#define V1_PIN 17     //Rele zona 1
+#define V2_PIN 13     //Rele zona 2
+#define V3_PIN 15     //Rele zona 3
+#define HUM1_PIN 39   //Pin sensor  capacitivo humedad z1
+#define HUM2_PIN 32   //Pin sensor  humedad z2
+#define HUM3_PIN 33   //Pin sensor  humedad z3
